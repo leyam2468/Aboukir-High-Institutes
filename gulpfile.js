34f26b5746d20f1fb29c085dfc0e2e9b5fbb55f8
Axel <leyamm3@gmail.com>
2020-03-14 07:41:11 +0200

    14/3 7:40
    Signed-off-by: Axel <leyamm3@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var prefix = require('gulp-autoprefixer');//for make outo brefixer css
 var sass = require('gulp-sass');//for comppuuler scss
 var pug = require('gulp-pug');//for compiler pug 
 var sourcemap = require('gulp-sourcemaps');//make map for file css
+var notify = require("gulp-notify");
 
 // html task
 gulp.task('html',function(){
     return gulp.src('input/pug/*.pug')
         .pipe(pug({pretty: true}))
         .pipe(gulp.dest('output'))
+        .pipe(notify("HTML!"));
 });
 
 // css Tastgulp-sourcemaps
@@ -21,6 +23,7 @@ gulp.task('css',function(){
         //.pipe(concat('main.css'))
         .pipe(sourcemap.write('.'))
         .pipe(gulp.dest('output/style/'))
+        .pipe(notify("css"));
 });
 
 // js Tast
@@ -28,13 +31,17 @@ gulp.task('js',function(){
     return gulp.src('input/js/*.js')
         .pipe(concat('main.js'))
         .pipe(gulp.dest('output/js'))
+        .pipe(notify("js"));
+
 });
+
 
 // watch task 
 gulp.task('watch',function(){
     gulp.watch('input/pug/**/*.pug',gulp.series('html'));
     gulp.watch('input/style/**/**.scss',gulp.series('css'));
     gulp.watch('input/js/**/**.js',gulp.series('js'));
+    //gulp.watch('input/img/**/**',gulp.series('image'));
 });
 // Default Task 
 gulp.task('default', gulp.parallel('watch'));
