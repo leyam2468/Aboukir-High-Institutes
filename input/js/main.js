338cb04c4e7ac2b7147d37089ab44a96c630a135
Axel <leyamm3@gmail.com>
2020-03-15 11:25:59 +0200

    15/3 11:25
    Signed-off-by: Axel <leyamm3@gmail.com>

---
+++ input/js/main.js
@@ -0,0 +1,9 @@
+//-typed area
+var typed = new Typed('.typing', {
+    strings:['','Management Information Systems','Tourism and Hotels','Monument Restoration'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop:true,
+    backDelay: 2500,
+    showCursor:false
+});
