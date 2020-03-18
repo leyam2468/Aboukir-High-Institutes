1a73b240f80d5823013fce5cdcf2f4fee61b4bda
leyam2468 <leyamm3@gmail.com>
2020-03-18 23:21:37 +0200

    18/ 3 11:21
    Signed-off-by: leyam2468 <leyamm3@gmail.com>

--- output/js/main.js
+++ output/js/main.js
@@ -1,3 +1,20 @@
+
+window.fbAsyncInit = function() {
+    FB.init({
+    xfbml            : true,
+    version          : 'v6.0'
+});
+};
+
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = 'https://connect.facebook.net/ar_AR/sdk/xfbml.customerchat.js';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+//- 
+    
 $(document).ready(function(){
     $('.toast').toast('show');
 });
