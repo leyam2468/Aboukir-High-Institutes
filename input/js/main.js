6621918fb228c1fe223d2ab66a16d9174a3b6e04
leyam2468 <leyamm3@gmail.com>
2020-03-21 18:08:51 +0200

    21/3 6:08
    Signed-off-by: leyam2468 <leyamm3@gmail.com>

--- input/js/main.js
+++ input/js/main.js
@@ -1,11 +1,18 @@
-
+//-nicescrol active
+$("body").niceScroll({
+    cursorcolor:"#ffc107",
+    cursorwidth:"4px",
+    zindex: "99999999",
+    cursorborder:"none",
+    scrollspeed: 100,
+});
+//--facebook chat active
 window.fbAsyncInit = function() {
     FB.init({
     xfbml            : true,
     version          : 'v6.0'
 });
 };
-
 (function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
@@ -13,14 +20,14 @@ js = d.createElement(s); js.id = id;
 js.src = 'https://connect.facebook.net/ar_AR/sdk/xfbml.customerchat.js';
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-//- 
+//--toast area
     
 $(document).ready(function(){
     $('.toast').toast('show');
 });
 //-typed area
 var typed = new Typed('.typing', {
-    strings:['','Management Information Systems','Tourism and Hotels','Monument Restoration'],
+    strings:['Management Information Systems','Tourism and Hotels','Monument Restoration'],
     typeSpeed: 50,
     backSpeed: 30,
     loop:true,
