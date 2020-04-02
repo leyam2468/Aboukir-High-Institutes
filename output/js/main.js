94b7a4ad82cdf1105308c059491277a8a183343e
leyam2468 <leyamm3@gmail.com>
2020-04-02 13:01:13 +0200

    2/4 1:01
    Signed-off-by: leyam2468 <leyamm3@gmail.com>

--- output/js/main.js
+++ output/js/main.js
@@ -36,9 +36,11 @@ var typed = new Typed('.typing', {
     backDelay: 3500,
     showCursor:false
 });
+
+
 $(document).ready(function(){
     $('.sclider-test').slick({
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
